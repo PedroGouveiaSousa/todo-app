ee928ea1239e7bf00b7c916ae60d3b400c2862f5
Pedro Sousa <PedroGouveiaSousa@Gmail.com>
2023-04-16 23:34:54 +0100

    localstorage functionality ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
 
 
-const [todos, setTodos] = createSignal();
+const [todos, setTodos] = createSignal(JSON.parse(localStorage.getItem('todos')) || []);
+
+let todoInput = null;
+
+const addTodo = () => {
+    if (todoInput.value.trim().length) {
+        setTodos([...todos(), {
+            id: todos().length,
+            text: todoInput.value,
+            completed: false
+        }]);
+        todoInput.value = '';
+        localStorage.setItem('todos', JSON.stringify(todos()));
+    }
+    console.log(todos());
+}
 
 
 const App = () => {
 
-    let todoInput = null;
+    onMount(() => {
+        todoInput.focus();
+    });
 
     return (
         <section class="section" style={{ "background-color": '#2f2e2e' }}>
             <div class="container">
-                <h1 class="title has-text-centered has-text-warning is-family-monospace">TODO LIST</h1>
+                <h1 class="title has-text-centered has-text-warning">TODO LIST</h1>
                 <div class="field has-addons">
                     <div class="control is-expanded">
                         <input
                             class="input"
                             ref={todoInput}
+                            onKeyUp={e => e.key === 'Enter' && addTodo()}
                         />
                     </div>
                     <div class="control">
                         <button
-                            class="button is-dark"
+                            class="button is-warning"
+                            onClick={e => addTodo(e)}
                         >Add</button>
                     </div>
                 </div>
+                <Show when={todos().length} fallback={<div>Loading ...</div>}>
+
+                </Show>
             </div>
         </section>
     );
